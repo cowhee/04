0c6ab5a1d75a30fa0359296e8153bc2d1e48384b
cowhee <71056160+cowhee@users.noreply.github.com>
2020-09-24 16:09:38 +0900

    2

--- main.c
+++ main.c
@@ -4,19 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x, y, z, m;
-	int a, b, c;
 	
-	x = 1;
-	a = 3; 
-	b = 4;
-	c = 5;
-	z = 2;
+	int a, b;
 	
-	y = a*x*x + b*x + c;
-	m = (x + y + z) / 3;
+	scanf("%d%d", &a, &b);
 	
-	printf("y = %d, m = %d", y, m);
+	printf("%d + %d = %d\n", a, b, a + b);
+	printf("%d - %d = %d\n", a, b, a - b);
+	printf("%d * %d = %d\n", a, b, a * b);
+	printf("%d / %d = %d\n", a, b, a / b);
+	printf("%d %% %d = %d\n", a, b, a % b);		
 	
 	return 0;
 }
